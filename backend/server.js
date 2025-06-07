7165e8ca1d5b28935ab4a7d5bfbdccbc650499bf
Yanis BOUCHAIB <yanisbouchaib28@gmail.com>
2025-06-07 13:55:00 +0200

    ajout like

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,15 @@ const postsRouter = require("./routes/posts");
 const messagesRouter = require("./routes/messages");
 const friendsRouter = require("./routes/friends");
 const suggestionsRouter = require("./routes/suggestions");
+const likesRoutes = require("./routes/likes");
+
 
 // ⚠️ Pas besoin de commentsRoutes séparé si c’est déjà géré dans /routes/posts.js
 app.use("/posts", postsRouter);
 app.use("/messages", messagesRouter);
 app.use("/friends", friendsRouter);
 app.use("/suggestions", suggestionsRouter);
+app.use("/api/posts", likesRoutes);
 
 // Lancement du serveur
 const PORT = 3001;
