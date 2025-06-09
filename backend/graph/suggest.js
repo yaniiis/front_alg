68c48ddd3a216eac40f85d404a4d9e7d09cadbe4
ahammiche <adelhammiche.pro@gmail.com>
2025-06-09 16:01:31 +0200

    fix

--- backend/graph/suggest.js
+++ backend/graph/suggest.js
@@ -1,5 +1,5 @@
-function suggestFriends(graph, friendOfFriendId) {
-  const user = graph.getUser(friendOfFriendId);
+function suggestFriends(graph, userId) {
+  const user = graph.getUser(userId);
   if (!user) return [];
 
   const suggestions = new Map();
@@ -7,15 +7,15 @@ function suggestFriends(graph, friendOfFriendId) {
   for (const friendId of user.friends) {
     const friend = graph.getUser(friendId);
     for (const friendOfFriendId of friend.friends) {
-      if (friendOfFriendId === friendOfFriendId || user.friends.has(friendOfFriendId)) continue;
+      if (friendOfFriendId === userId || user.friends.has(friendOfFriendId)) continue;
 
-      const friendOfFriend = graph.getUser(friendOfFriendId);
+      const fof = graph.getUser(friendOfFriendId);
       let score = 1;
 
-      const commonInterests = [...user.interests].filter(i => friendOfFriend.interests.has(i));
-      score += commonInterests.length * 2;
+      const commonInterests = [...user.interests].filter(i => fof.interests.has(i));
+      score += commonInterests.length;
 
-      if (user.interactions.has(friendOfFriendId) || friendOfFriend.interactions.has(friendOfFriendId)) score += 2;
+      if (user.interactions.has(friendOfFriendId) || fof.interactions.has(userId)) score += 2;
 
       suggestions.set(friendOfFriendId, (suggestions.get(friendOfFriendId) || 0) + score);
     }
@@ -23,6 +23,7 @@ function suggestFriends(graph, friendOfFriendId) {
 
   return [...suggestions.entries()]
     .sort((a, b) => b[1] - a[1])
+    .slice(0, topN)
     .map(([id]) => graph.getUser(id));
 }
 
